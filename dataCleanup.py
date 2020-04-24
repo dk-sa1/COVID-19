5099a5462380c8f6b2bf0b07496b2ffe751b037a
Michael Salinas <misalina@microsoft.com>
2020-04-23 20:34:43 -0700

    polynomial model

--- dataCleanup.py
+++ dataCleanup.py
@@ -1,6 +1,7 @@
-import statsmodels as ss
+import statsmodels.api as sm
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
 import pandas as pd 
 import matplotlib
 from matplotlib import pyplot
@@ -8,36 +9,72 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 dataIn = pd.read_csv ('csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv')
-# print(dataIn[0:5])
 
-# unitedStates = dataIn[dataIn.Country_Region == "US"]
-unitedStates = dataIn.query('Country_Region == "US"') #filter to just united states
-unitedStates = unitedStates[unitedStates.columns[10:]] #remove unnecessary columns
-unitedStates = unitedStates.T 
+# unitedStates = dataIn.query('Country_Region == "US"') #filter to just united states
+unitedStates = dataIn[dataIn.columns[10:]] #remove unnecessary columns
+unitedStates = unitedStates.T # transpose the data set
 unitedStates.columns = unitedStates.iloc[0] # uses labels as the column headers
 unitedStates = unitedStates.iloc[1:, :] # remove the first row of column headers
-unitedStates.reset_index()
+# unitedStates.reset_index()
 # unitedStates = unitedStates.reindex(unitedStates.index.drop('Combined_Key'))
 # # unitedStates = unitedStates.loc[:, (unitedStates.sum() > 10000)] #filter to cities that add up to more than 10000
-unitedStates['Sum Cities'] = unitedStates.sum(axis=1) # add total column field
-unitedStatesTotal = unitedStates.iloc[:, [-1]] # filter to just the total column 
+unitedStates['US Total'] = unitedStates.sum(axis=1) # add total column field
+unitedStatesTotal = unitedStates.iloc[:, [-1]] # filter to just the total column
 
-# x = unitedStatesTotal.index
-# y = unitedStates.iloc[:, 1]
+modelUnitedStates = unitedStatesTotal
+modelUnitedStates.index = range(0, len(modelUnitedStates))
 
+x = modelUnitedStates.index
+x = np.array(x).reshape(-1,1) # need to reshape the index data because it expects multiple columns to look at. not just one.
+y = modelUnitedStates.iloc[:, 0]
+# print(x)
+# print(y)
+
+### Linear Regression Example
 # model = LinearRegression()
 # model = model.fit(x, y)
 
+# intercept = model.intercept_
+# slope = model.coef_
 # r_sq = model.score(x, y)
-# print(r_sq)
+# print('intercept: ', intercept)
+# print('slope: ', slope)
+# print('r^2: ', r_sq)
 
-# print(unitedStates)
-# print(list(unitedStates.columns[:10]))
-# print(unitedStates.index[:3])
+### Polynomial Regression Example
+# transformer = PolynomialFeatures(degree=2, include_bias=False) #transform the input array to contain the additional column(s) with the values of x^2 and eventually more.
+# transformer.fit(x) # need to fit it before applying to the model
+# x_transformed = transformer.transform(x) # modify the input
 
-# unitedStates.plot()
+# # model = LinearRegression(fit_intercept=False).fit(x_transformed, y)
+# model = LinearRegression().fit(x_transformed, y)
+
+# intercept = model.intercept_
+# slope = model.coef_
+# r_sq = model.score(x_transformed, y)
+# print('intercept: ', intercept)
+# print('coefficient(s): ', slope)
+# print('r^2: ', r_sq)
+
+### stats oriented example
+transformer = PolynomialFeatures(degree=2, include_bias=False) #transform the input array to contain the additional column(s) with the values of x^2 and eventually more.
+transformer.fit(x) # need to fit it before applying to the model
+x_transformed = transformer.transform(x) # modify the input
+x_transformed = sm.add_constant(x_transformed)
+
+model = sm.OLS(y, x_transformed)
+results = model.fit()
+
+unitedStatesTotal.plot()
+pyplot.show()
+
+print(results.summary())
+
+# unitedStatesTotal.plot()
 # pyplot.show()
 
-print(unitedStates.head())
+# print(unitedStates.head())
+# print(unitedStatesTotal.head())
+
 # print(type(unitedStates))
 # print('done')
